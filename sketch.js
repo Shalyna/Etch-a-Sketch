d5d15c8c685b547b1412a5f25478f55a4188408b
“SamR248” <r.shalyna@gmail.com>
2023-02-14 15:31:42 -0800

    added EventListener for mouseover can now draw on grid in black

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 const body = document.body;
 const container = document.createElement('div');
+
 container.className = 'container';
+
 body.append(container);
 
 container.style.backgroundColor = "white";
@@ -11,8 +13,11 @@ container.style.height = "384px";
 body.style.backgroundColor = "aquamarine";
 body.style.minHeight = "100vh";
 
-for (let i = 1; i <= 16 * 16; i++) {
-    let gridBox = document.createElement('div');
-    container.appendChild(gridBox);
+    for (let i = 1; i <= 16 * 16; i++) {
+    const gridBox = document.createElement('div');
+    gridBox.addEventListener('mouseover', () => {
+        gridBox.style.backgroundColor = 'black';
+    });
     gridBox.style.border = "1px solid darkgrey";
+    container.appendChild(gridBox);
 }
